26bb2be06b17753baf7e61b86cc3e6e62765355c
sumit <sumit@gmail.com>
2024-12-10 00:18:31 +0530

    adding forms

--- Routes/users.js
+++ Routes/users.js
@@ -4,30 +4,6 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const User = require("../models/user");
 
-// router.get("/signup", (req, res) => {
-//     res.render("./users/signup.ejs");
-// });
-
-// router.post("/signup", async (req, res) => {
-//     try {
-//         let { username, email, password } = req.body;
-//         const newUser = new User({ username, email });
-//         const registeredUser = await User.register(newUser, password);
-//         console.log(registeredUser);
-//         req.flash("success", "welcome to wanderlust");
-//         res.redirect("/listings");
-//     } catch (e) {
-//         req.flash("error", e.message);
-//         console.log(e.message);
-//         res.redirect("/signup");
-//     }
-// });
-
-// router.get("/login", (req, res) => {
-//     res.render("./users/login.ejs");
-// });
-
-// module.exports = router;
 
 router.get("/signup", (req, res) => {
     res.render("./users/signup.ejs");
